e23eec88c98ec7d7facbe786ca24d348014d3489
Uma Sahni <monkeytown64@gmail.com>
2023-08-29 17:49:19 +0530

    Atleast till here it is working

--- Backend/renewproject/renewapp/urls.py
+++ Backend/renewproject/renewapp/urls.py
@@ -4,6 +4,6 @@ from .views import user_registration,user_login
 urlpatterns = [
     path('register/', user_registration, name='user_registration'),
     path('login/', user_login, name='user_login'),
-    # path('get-csrf-token/', get_csrf_token, name='get-csrf-token'),
+    
     # Other URL patterns can be added here
 ]
